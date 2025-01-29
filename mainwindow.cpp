b289eb1719028251a5d7148619a0e5f28e5135d0
imad-miftek <imad@miftek.com>
2025-01-29 08:53:35 -0500

    hello user dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"Hello",
+                             QString("Hello " + ui->lineEdit->text()));
+}
+
